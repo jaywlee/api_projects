0e67c4d5d709075b4df5a90439060b90683606d1
jaywlee <jaywlee@chicagobooth.edu>
2014-10-29 00:01:21 -0500

    API 2

--- address_to_weather.rb
+++ address_to_weather.rb
@@ -1,11 +1,12 @@
 require 'open-uri'
 require 'json'
 
+
 # If you experience an error relating to SSL,
 #   uncomment the following two lines:
 
-# require 'openssl'
-# OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+#require 'openssl'
+#OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 puts "Let's get the weather forecast for your address."
 
@@ -15,8 +16,25 @@ url_safe_address = URI.encode(the_address)
 
 # Your code goes below.
 
+url_of_data_we_want = "http://maps.googleapis.com/maps/api/geocode/json?address="
+url = url_of_data_we_want + url_safe_address
+raw_data = open(url).read
+parsed_data = JSON.parse(raw_data)
+the_latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+the_longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+
+url_of_data_we_want = "https://api.forecast.io/forecast/a432dda20e513555718f2527e386944c/"
+url = url_of_data_we_want + the_latitude + "," + the_longitude
+raw_data = open(url).read
+parsed_data = JSON.parse(raw_data)
+the_temperature = parsed_data["currently"]["temperature"]
+the_hour_outlook = parsed_data["hourly"]["data"][1]["summary"]
+the_day_outlook = parsed_data["daily"]["data"][1]["summary"]
+
+
 # Ultimately, we want the following line to work when uncommented:
 
-# puts "The current temperature at #{the_address} is #{the_temperature} degrees."
-# puts "The outlook for the next hour is: #{the_hour_outlook}"
-# puts "The outlook for the next day is: #{the_day_outlook}"
+puts "The current temperature at #{the_address} is #{the_temperature} degrees."
+puts "The outlook for the next hour is: #{the_hour_outlook}"
+puts "The outlook for the next day is: #{the_day_outlook}"
+
